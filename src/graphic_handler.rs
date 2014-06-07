e02e422dbe241d363a605ea527c69c8ca6f894d0
imperio <imperio@imperio-HP-EliteBook-8540p-NU486AV.(none)>
2014-06-07 15:57:30 +0200

    A lot of changes : - I added the spectrum - New keys are handled like :  * +/- to change sound volume  * up/down to change music - The design is a little more pretty (a little)

--- src/graphic_handler.rs
+++ src/graphic_handler.rs
@@ -33,7 +33,47 @@ use std::rc::Rc;
 use std::cell::RefCell;
 
 struct GraphicSpectrum {
-    line: rc::RectangleShape
+    spectrum: Vec<rc::RectangleShape>,
+    height: uint
+}
+
+impl GraphicSpectrum {
+    fn init(mut self) -> GraphicSpectrum {
+        let mut it = 0;
+
+        while it < 512 {
+            self.spectrum.push(match rc::RectangleShape::new_init(&Vector2f{x: 1f32, y: self.height as f32}) {
+                Some(l) => l,
+                None => fail!("Cannot create spectrum")
+            });
+            self.spectrum.get_mut(it).set_fill_color(&Color::new_RGB(50, 100, 30));
+            self.spectrum.get_mut(it).set_position(&Vector2f{x: it as f32, y: self.height as f32});
+            it += 1;
+        }
+        self
+    }
+
+    fn new(height: uint) -> GraphicSpectrum {
+        GraphicSpectrum {
+            spectrum: Vec::new(),
+            height: height
+        }.init()
+    }
+
+    fn update_spectrum(&mut self, data: Vec<f32>) {
+        let mut it = 0u;
+
+        for tmp in data.iter() {
+            self.spectrum.get_mut(it).set_size(&Vector2f{x: 1f32, y: self.height as f32 * *tmp * -20f32});
+            it += 1;
+        }
+    }
+
+    fn draw(&mut self, win: &mut RenderWindow) {
+        for tmp in self.spectrum.mut_iter() {
+            win.draw(tmp);
+        }
+    }
 }
 
 struct GraphicPlayList {
@@ -42,7 +82,8 @@ struct GraphicPlayList {
     graphic_size: Vector2u,
     position: Vector2u,
     to_draw: uint,
-    current: uint
+    current: uint,
+    border: rc::RectangleShape
 }
 
 impl GraphicPlayList {
@@ -66,7 +107,11 @@ impl GraphicPlayList {
             graphic_size: Vector2u{x: 0u32, y: 0u32},
             position: Vector2u{x: 0u32, y: 0u32},
             to_draw: 0u,
-            current: 1u
+            current: 1u,
+            border: match rc::RectangleShape::new_init(&Vector2f{x: 0f32, y: 1f32}) {
+                Some(l) => l,
+                None => fail!("Cannot create border for GraphicPlayList")
+            }
         }.init(font)
     }
 
@@ -77,7 +122,11 @@ impl GraphicPlayList {
             graphic_size: size.clone(),
             position: position.clone(),
             to_draw: 0u,
-            current: 1u
+            current: 1u,
+            border: match rc::RectangleShape::new_init(&Vector2f{x: 1f32, y: size.y as f32}) {
+                Some(l) => l,
+                None => fail!("Cannot create border for GraphicPlayList")
+            }
         }.init(font)
     }
 
@@ -87,6 +136,7 @@ impl GraphicPlayList {
 
         self.position = position.clone();
         self.to_draw = 0;
+        self.border.set_position(&Vector2f{x: position.x as f32 - 1f32, y: position.y as f32});
         for tmp in self.texts.mut_iter() {
             tmp.set_position(&Vector2f{x: self.position.x as f32, y: pos as f32 + self.position.y as f32});
             if pos < limit {
@@ -106,12 +156,13 @@ impl GraphicPlayList {
             win.draw(tmp);
             it += 1;
         }
+        win.draw(&self.border);
     }
 
     fn set_current(&mut self, current: uint) {
         if current != self.current {
             self.texts.get_mut(current).set_color(&Color::new_RGB(255, 125, 25));
-            self.texts.get_mut(self.current).set_color(&Color::new_RGB(255, 2555, 255));
+            self.texts.get_mut(self.current).set_color(&Color::new_RGB(255, 255, 255));
             self.current = current;
         }
     }
@@ -121,24 +172,75 @@ impl GraphicPlayList {
         let tmp = self.position;
         self.set_position(&tmp);
     }
+
+    fn is_inside(&self, pos: &Vector2u) -> bool {
+        pos.y >= self.position.y && pos.y <= self.position.y + self.graphic_size.y &&
+        pos.x >= self.position.x && pos.y <= self.position.x + self.graphic_size.x
+    }
+}
+
+struct GraphicTimer {
+    timer: rc::Text,
+    cleaner: rc::RectangleShape,
+    position: Vector2u
+}
+
+impl GraphicTimer {
+    fn new(font: Font, size: &Vector2u, position: &Vector2u) -> GraphicTimer {
+        GraphicTimer {
+            timer: match rc::Text::new_init("", Rc::new(RefCell::new(font)), 20) {
+                Some(t) => t,
+                None => fail!("Cannot create GraphicTimer")
+            },
+            cleaner: match rc::RectangleShape::new_init(&Vector2f{x: size.x as f32 + 1f32, y: size.y as f32 + 1f32}) {
+                Some(l) => l,
+                None => fail!("Cannot create cleaner for GraphicTimer")
+            },
+            position: position.clone()
+        }.init()
+    }
+
+    fn init(mut self) -> GraphicTimer {
+        let tmp = self.position.clone();
+        self.set_position(&tmp);
+        self.cleaner.set_fill_color(&Color::new_RGB(0, 0, 0));
+        self.cleaner.set_outline_color(&Color::new_RGB(255, 255, 255));
+        self.cleaner.set_outline_thickness(1f32);
+        self
+    }
+
+    fn set_position(&mut self, position: &Vector2u) {
+        self.cleaner.set_position(&Vector2f{x: self.position.x as f32, y: self.position.y as f32});
+        self.timer.set_position(&Vector2f{x: self.position.x as f32 + 5f32, y: self.position.y as f32 + 2f32});
+    }
+
+    fn draw(&self, win: &mut RenderWindow) {
+        win.draw(&self.cleaner);
+        win.draw(&self.timer);
+    }
 }
 
 struct ProgressBar {
     line: rc::RectangleShape,
-    progress_size: Vector2u,
+    graphic_size: Vector2u,
     maximum: uint,
     value: uint,
-    real_value: uint
+    real_value: uint,
+    cleaner: rc::RectangleShape,
 }
 
 impl ProgressBar {
-    fn new<T>(color: &Color) -> ProgressBar {
+    fn new(color: &Color) -> ProgressBar {
         ProgressBar {
             line: rc::RectangleShape::new().unwrap(),
-            progress_size: Vector2u{x: 0, y: 0},
+            graphic_size: Vector2u{x: 0, y: 0},
             maximum: 1,
             value: 0u,
-            real_value: 0u
+            real_value: 0u,
+            cleaner: match rc::RectangleShape::new_init(&Vector2f{x: 0f32, y: 1f32}) {
+                Some(l) => l,
+                None => fail!("Cannot create border for ProgressBar")
+            }
         }.init(color, &Vector2u{x: 0, y: 0})
     }
 
@@ -148,62 +250,83 @@ impl ProgressBar {
                 Some(l) => l,
                 None => fail!("Cannot create progress bar")
             },
-            progress_size: size.clone(),
+            graphic_size: size.clone(),
             maximum: maximum,
             value: 0u,
-            real_value: 0u
+            real_value: 0u,
+            cleaner: match rc::RectangleShape::new_init(&Vector2f{x: size.x as f32 + 1f32, y: size.y as f32 + 1f32}) {
+                Some(l) => l,
+                None => fail!("Cannot create border for ProgressBar")
+            }
         }.init(color, position)
     }
 
     fn init(mut self, color: &Color, position: &Vector2u) -> ProgressBar {
-        self.line.set_position(&Vector2f{x: position.x as f32, y: position.y as f32});
+        self.set_position(position);
         self.line.set_fill_color(color);
+        self.cleaner.set_fill_color(&Color::new_RGB(0, 0, 0));
+        self.cleaner.set_outline_color(&Color::new_RGB(255, 255, 255));
+        self.cleaner.set_outline_thickness(1f32);
         self
     }
 
     fn draw(&self, win: &mut RenderWindow) {
+        win.draw(&self.cleaner);
         win.draw(&self.line);
     }
 
     fn set_size(&mut self, size: &Vector2u) {
-        self.progress_size = size.clone();
-        self.line.set_size(&Vector2f{x: 0f32, y: size.y as f32});
-        self.maximum = size.x as uint;
+        self.graphic_size = size.clone();
+        self.cleaner.set_size(&Vector2f{x: size.x as f32 + 1f32, y: size.y as f32 + 1f32});
+        self.set_progress(self.real_value);
     }
 
     fn set_position(&mut self, position: &Vector2u) {
         self.line.set_position(&Vector2f{x: position.x as f32, y: position.y as f32});
+        self.cleaner.set_position(&Vector2f{x: position.x as f32 - 1f32, y: position.y as f32 - 1f32});
     }
 
     fn set_progress(&mut self, position: uint) {
-        let new_value = position * self.progress_size.x as uint / self.maximum;
+        let tmp = if position > self.maximum {
+            self.maximum
+        } else {
+            position
+        };
+        let new_value = tmp * self.graphic_size.x as uint / self.maximum;
 
         if new_value != self.value {
             self.value = new_value;
             self.real_value = position;
-            self.line.set_size(&Vector2f{x: self.value as f32, y: self.progress_size.y as f32});
+            self.line.set_size(&Vector2f{x: self.value as f32, y: self.graphic_size.y as f32});
         }
     }
 
-    fn click(&mut self, pos: &Vector2u, width: u32) {
-        let in_order = (pos.x as u32 - self.line.get_position().x as u32) * self.progress_size.x as u32 / width;
+    fn click(&mut self, pos: &Vector2u) {
+        let in_order = (pos.x as f32 - self.line.get_position().x) / self.graphic_size.x as f32 * 100f32;
 
-        self.set_progress(in_order as uint * self.maximum / self.progress_size.x as uint);
+        self.set_progress((in_order * self.maximum as f32 / 100f32) as uint);
+    }
+
+    fn is_inside(&self, pos: &Vector2u) -> bool {
+        pos.y >= self.line.get_position().y as u32 && pos.y <= self.line.get_position().y as u32 + self.graphic_size.y &&
+        pos.x >= self.line.get_position().x as u32 && pos.y <= self.line.get_position().x as u32 + self.graphic_size.x
     }
 }
 
 pub struct GraphicHandler {
     font: Font,
     musics: GraphicPlayList,
-    pub timer: rc::Text,
+    timer: GraphicTimer,
     music_bar: ProgressBar,
     volume_bar: ProgressBar,
     playlist: PlayList,
+    spectrum: GraphicSpectrum
 }
 
 impl GraphicHandler {
     fn init(mut self) -> GraphicHandler {
-        //self.timer.set_progress(&Vector2f{x: 0f32, y: 25f32});
+        self.volume_bar.set_progress(100);
+        self.timer.set_position(&Vector2u{x: 513u32, y: self.volume_bar.line.get_position().y as u32});
         self
     }
 
@@ -215,18 +338,17 @@ impl GraphicHandler {
         GraphicHandler {
             font: font.clone(),
             musics: GraphicPlayList::new_init(playlist.to_vec(), &font,
-                &Vector2u{x: window.get_size().x - (window.get_size().x - 512u32), y: 0},
+                &Vector2u{x: window.get_size().x - (window.get_size().x - 513u32), y: 0},
                 &Vector2u{x: window.get_size().x - 512u32, y: window.get_size().y - 20u32}),
-            timer: match rc::Text::new_init("", Rc::new(RefCell::new(font)), 20) {
-                Some(t) => t,
-                None => fail!("Cannot create Text")
-            },
+            timer: GraphicTimer::new(font, &Vector2u{x: window.get_size().x - 632u32, y: 24u32},
+                                        &Vector2u{x: 513u32, y: window.get_size().y - 35u32}),
             music_bar: ProgressBar::new_init(&Vector2u{x: window.get_size().x, y: 8u32}, &Vector2u{x: 0u32, y: window.get_size().y - 8u32},
                 &Color::new_RGB(255, 255, 255), 1u),
-            volume_bar: ProgressBar::new_init(&Vector2u{x: window.get_size().x / 5, y: 10u32},
-                &Vector2u{x: window.get_size().x - window.get_size().x / 5, y: window.get_size().y - 19u32},
+            volume_bar: ProgressBar::new_init(&Vector2u{x: 120u32, y: 20u32},
+                &Vector2u{x: window.get_size().x - 120u32, y: window.get_size().y - 29u32},
                 &Color::new_RGB(255, 25, 25), 100u),
-            playlist: playlist
+            playlist: playlist,
+            spectrum: GraphicSpectrum::new(window.get_size().y as uint - 9u)
         }.init()
     }
 
@@ -257,9 +379,10 @@ impl GraphicHandler {
     pub fn update(&mut self, win: &mut RenderWindow) {
         win.clear(&Color::new_RGB(0, 0, 0));
         self.musics.draw(win);
-        win.draw(&self.timer);
-        self.music_bar.draw(win);
         self.volume_bar.draw(win);
+        self.timer.draw(win);
+        self.spectrum.draw(win);
+        self.music_bar.draw(win);
         win.display();
     }
 
@@ -270,7 +393,8 @@ impl GraphicHandler {
                     let position = chan.get_position(FMOD_TIMEUNIT_MS).unwrap();
 
                     if position != old_position {
-                        self.timer.set_string(format!("{:02u}:{:02u} / {:02u}:{:02u}",
+                        self.spectrum.update_spectrum(chan.get_spectrum(512u, 0i32, fmod::DSP_FFT_WindowRect).unwrap());
+                        self.timer.timer.set_string(format!("{:02u}:{:02u} / {:02u}:{:02u}",
                             position / 1000 / 60, position / 1000 % 60, length / 1000 / 60, length / 1000 % 60).as_slice());
                         Some(position)
                     } else {
@@ -329,11 +453,25 @@ impl GraphicHandler {
                         }
                         _ => {}
                     },
+                    event::KeyPressed{code, ..} => match code {
+                        keyboard::Add => {
+                            self.volume_bar.set_progress(self.volume_bar.real_value + 1);
+                            chan.set_volume(self.volume_bar.real_value as f32 / 100f32);
+                        }
+                        keyboard::Substract => {
+                            self.volume_bar.set_progress(self.volume_bar.real_value - 1);
+                            chan.set_volume(self.volume_bar.real_value as f32 / 100f32);
+                        }
+                        _ => {}
+                    },
                     event::MouseButtonReleased{button, x, y} => match button {
                         mouse::MouseLeft => {
-                            if y >= window.get_size().y as int - self.music_bar.progress_size.y as int {
-                                self.music_bar.click(&Vector2u{x: x as u32, y: y as u32}, window.get_size().x);
+                            if self.music_bar.is_inside(&Vector2u{x: x as u32, y: y as u32}) {
+                                self.music_bar.click(&Vector2u{x: x as u32, y: y as u32});
                                 chan.set_position(self.music_bar.real_value, FMOD_TIMEUNIT_MS);
+                            } else if self.volume_bar.is_inside(&Vector2u{x: x as u32, y: y as u32}) {
+                                self.volume_bar.click(&Vector2u{x: x as u32, y: y as u32});
+                                chan.set_volume(self.volume_bar.real_value as f32 / 100f32);
                             }
                         },
                         _ => {}
